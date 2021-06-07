f335b7ff76c31716a94eb36e1c9e030da138c3b4
2 u <info247@gmail.com>
2021-06-07 12:57:41 +0200

    fix capture photo canvas width

--- webrtc-capturestill/capture.js
+++ webrtc-capturestill/capture.js
@@ -5,7 +5,6 @@
 
   var width = 320;    // We will scale the photo width to this
   var height = 0;     // This will be computed based on the input stream
-  //var height = 240;     // This will be computed based on the input stream
 
   // |streaming| indicates whether or not we're currently streaming
   // video from the camera. Obviously, we start at false.
